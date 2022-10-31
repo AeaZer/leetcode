cec276d3836e1aa2292dc471be249725f4ef7ab1
hewenchang <3474974468@qq.com>
2022-10-31 15:54:25 +0800

    ...

--- middle/generateMatrix.go
+++ middle/generateMatrix.go
@@ -1,6 +1,52 @@
 package main
 
-func generateMatrix(n int) (res [][]int) {
+const incrementLoop = iota + 1
 
-	return nil
+type matrixParams struct {
+	matrixNum int
+	finalXY   [2]int
+	x         int
+	y         int
+	xLoop     *LoopParams
+	yLoop     *LoopParams
+}
+
+type LoopParams struct {
+	Increment int
+	LoopNum   int
+}
+
+func newMatrixParams(matrixNum int) *matrixParams {
+	m := new(matrixParams)
+	m.matrixNum = matrixNum
+	var finalXY [2]int
+	if matrixNum%2 == 0 {
+		finalXY[0] = matrixNum%2 + 1
+		finalXY[1] = matrixNum%2 - 1
+	} else {
+		finalXY[0] = (matrixNum + 1) % 2
+		finalXY[1] = (matrixNum + 1) % 2
+	}
+	m.finalXY = finalXY
+	return m
+}
+
+func (p *matrixParams) generateLoop() [][]int {
+	res := make([][]int, p.matrixNum)
+	for i, _ := range res {
+		subArray := make([]int, p.matrixNum)
+		for j := 0; j < p.matrixNum; j++ {
+			subArray[j] = 0
+		}
+		res[i] = subArray
+	}
+	count := 0
+	for count <= p.matrixNum*p.matrixNum {
+
+	}
+	return res
+}
+
+func generateMatrix(n int) [][]int {
+	return newMatrixParams(n).generateLoop()
 }

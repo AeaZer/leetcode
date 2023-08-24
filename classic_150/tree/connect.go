8ade5eaaef1e2e83d1aeee955ded4ce374477583
Aeazer <3474974468@qq.com>
2023-08-24 09:25:46 +0800

    tree to linklist

--- classic_150/tree/connect.go
+++ classic_150/tree/connect.go
@@ -1,5 +1,11 @@
 package tree
 
+// 117. 填充每个节点的下一个右侧节点指针 II
+// https://leetcode.cn/problems/populating-next-right-pointers-in-each-node-ii/
+
+/*填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL 。
+初始状态下，所有 next 指针都被设置为 NULL 。*/
+
 type Node struct {
 	Val   int
 	Left  *Node

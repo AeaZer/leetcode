bbdab544d862d37dc8032aef25a06e43593c52ef
AeaZer <3474974468@qq.com>
2023-08-28 17:17:53 +0800

    130. 被围绕的区域

---
+++ classic_150/graph/solve_test.go
@@ -0,0 +1,8 @@
+package graph
+
+import "testing"
+
+func TestSolve(t *testing.T) {
+	board := [][]byte{{'X', 'O', 'X', 'O', 'X', 'O'}, {'O', 'X', 'O', 'X', 'O', 'X'}, {'X', 'O', 'X', 'O', 'X', 'O'}, {'O', 'X', 'O', 'X', 'O', 'X'}}
+	solve(board)
+}

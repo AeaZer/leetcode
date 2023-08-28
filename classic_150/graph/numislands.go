f9c7c45349a69682f54311939f6849ef903e5fb5
AeaZer <3474974468@qq.com>
2023-08-28 13:39:19 +0800

    200. 岛屿数量

--- classic_150/graph/numislands.go
+++ classic_150/graph/numislands.go
@@ -7,14 +7,47 @@ package graph
 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
 此外，你可以假设该网格的四条边均被水包围。*/
 
+/*m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] 的值为 '0' 或 '1'*/
+
 const (
-	spaceWater byte = iota
-	spaceLand
+	spaceWater   byte = '0'
+	spaceLand    byte = '1'
+	spaceVisited byte = '2'
 )
 
 func numIslands(grid [][]byte) int {
 	var res int
 	iMax, jMax := len(grid), len(grid[0])
-
+	var dfs func(rowIndex, columnIndex int)
+	dfs = func(rowIndex, columnIndex int) {
+		// 下标不在给定的区域内
+		if !inArea(iMax, jMax, rowIndex, columnIndex) {
+			return
+		}
+		// 如果此节点是"水"或者"已被访问过"那就跳出递归
+		if grid[rowIndex][columnIndex] == spaceWater || grid[rowIndex][columnIndex] == spaceVisited {
+			return
+		}
+		grid[rowIndex][columnIndex] = spaceVisited // grid[rowIndex][columnIndex] == spaceLand
+		dfs(rowIndex+1, columnIndex)               // 右边的节点
+		dfs(rowIndex-1, columnIndex)               // 左边的节点
+		dfs(rowIndex, columnIndex+1)               // 下边的节点
+		dfs(rowIndex, columnIndex-1)               // 上面的节点
+	}
+	for i := 0; i < iMax; i++ {
+		for j := 0; j < jMax; j++ {
+			if grid[i][j] == spaceLand {
+				res++
+				dfs(i, j)
+			}
+		}
+	}
 	return res
+}
+
+func inArea(iMax, jMax, rowIndex, columnIndex int) bool {
+	return rowIndex < iMax && rowIndex >= 0 && columnIndex < jMax && columnIndex >= 0
 }
---
+++ classic_150/graph/solve.go
@@ -0,0 +1,10 @@
+package graph
+
+// 130. 被围绕的区域
+// https://leetcode.cn/problems/surrounded-regions/
+
+/*给你一个 m x n 的矩阵 board ，由若干字符 'X' 和 'O' ，找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。*/
+
+func solve(board [][]byte) {
+
+}

4529a7b068798ab4a3dac7c7e6e5e11cf733f95f
AeaZer <3474974468@qq.com>
2023-08-30 13:07:37 +0800

    课程表

--- classic_150/graph/canfinish_test.go
+++ classic_150/graph/canfinish_test.go
@@ -1,8 +1,9 @@
 package graph
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCanFinish(t *testing.T) {
@@ -14,6 +15,6 @@ func TestCanFinish(t *testing.T) {
 	finish = canFinish(3, [][]int{{1, 4}})
 	assert.True(finish)
 
-	finish = canFinish(7, [][]int{{1, 0}, {0, 3}, {0, 2}, {3, 2}, {2, 5}, {4, 5}, {5, 6}, {2, 4}})
+	finish = canFinish2(7, [][]int{{1, 0}, {0, 3}, {0, 2}, {3, 2}, {2, 5}, {4, 5}, {5, 6}, {2, 4}})
 	assert.True(finish)
 }
---
+++ classic_150/graph/findorder.go
@@ -0,0 +1,7 @@
+package graph
+
+func findOrder(numCourses int, prerequisites [][]int) []int {
+	topoMap = make(map[int][]int, 0).
+
+	return []int{}
+}

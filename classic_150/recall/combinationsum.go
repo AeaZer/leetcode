03500b644cfee5c7e5e55bf5fa2040173a2d6a6c
AeaZer <3474974468@qq.com>
2023-09-25 15:57:19 +0800

    39. 组合总和

--- classic_150/recall/combinationsum.go
+++ classic_150/recall/combinationsum.go
@@ -3,27 +3,27 @@ package recall
 import "sort"
 
 func combinationSum(candidates []int, target int) [][]int {
-	sort.Ints(candidates)
-	var (
-		res       = make([][]int, 0)
-		backtrack func([]int, int)
-	)
-	backtrack = func(path []int, index int) {
-		if candidates[index] > target {
-			return
-		}
-		if candidates[index]-target == 0 {
-			comb := make([]int, len(path))
-			copy(comb, path)
-			res = append(res, comb)
-			return
-		}
-		path = append(path, candidates[index])
-		for i := 0; i < len(candidates); i++ {
-			backtrack(path, i)
-			path = path[:len(path)-1]
+    sort.Ints(candidates)
+	length := len(candidates)
+	res := make([][]int, 0)
+	var callback func([]int, int, int)
+	callback = func(arr []int, sum, index int) {
+		for i := index; i < length; i++ {
+			arr = append(arr, candidates[i])
+			sum += candidates[i]
+			if sum == target {
+				comb := make([]int, len(arr))
+				copy(comb, arr)
+				res = append(res, comb)
+			}
+			if sum > target {
+				return
+			}
+			callback(arr, sum, i)
+			arr = arr[:len(arr)-1]
+			sum -= candidates[i]
 		}
 	}
-	backtrack([]int{}, 0)
+	callback([]int{}, 0, 0)
 	return res
 }

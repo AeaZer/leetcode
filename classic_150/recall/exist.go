4a54c96dae7711461d10cbae7268ec26d4f37138
AeaZer <3474974468@qq.com>
2023-09-26 16:04:27 +0800

    79. 单词搜索

--- classic_150/recall/exist.go
+++ classic_150/recall/exist.go
@@ -1,29 +1,78 @@
 package recall
 
+const (
+	stateFail = iota
+	stateNext
+)
+
 func exist(board [][]byte, word string) bool {
 	wordBytes := []byte(word)
 	var (
 		bl     = len(wordBytes)
-		rowL   = len(board)
-		colL   = len(board[0])
+		colL   = len(board)
+		rowL   = len(board[0])
 		found  bool
-		search func(int, int, int)
+		search func(int, int, int, int)
 	)
-	search = func(rowIndex, colIndex, index int) {
-		if found || rowIndex >= rowL || colIndex >= colL {
+	if bl > colL*rowL {
+		return false
+	}
+	visited := make([][]bool, colL)
+	for i := range visited {
+		visited[i] = make([]bool, rowL)
+	}
+	search = func(i, j, index, state int) {
+		if found {
 			return
 		}
-		if board[rowIndex][colIndex] == wordBytes[index] {
+		if board[i][j] == wordBytes[index] {
 			if index == bl-1 {
 				found = true
 				return
 			}
-			search(rowIndex, colIndex-1, index+1)
-			search(rowIndex, colIndex+1, index+1)
-			search(rowIndex+1, colIndex, index+1)
+			// 向左查找
+			if j-1 >= 0 && !visited[i][j-1] {
+				visited[i][j-1] = true
+				search(i, j-1, index+1, stateNext)
+				visited[i][j-1] = false
+			}
+
+			// 向右查找
+			if j+1 < rowL && !visited[i][j+1] {
+				visited[i][j+1] = true
+				search(i, j+1, index+1, stateNext)
+				visited[i][j+1] = false
+			}
+
+			// 向下查找
+			if i+1 < colL && !visited[i+1][j] {
+				visited[i+1][j] = true
+				search(i+1, j, index+1, stateNext)
+				visited[i+1][j] = false
+			}
+
+			// 向上查找
+			if i-1 >= 0 && !visited[i-1][j] {
+				visited[i-1][j] = true
+				search(i-1, j, index+1, stateNext)
+				visited[i-1][j] = false
+			}
 		}
-		search(rowIndex, colIndex+1, index)
-		search(rowIndex+1, colIndex, index)
+		if state == stateNext {
+			return
+		}
+		// 查找第一个
+		if i+1 < colL {
+			visited[i+1][j] = true
+			search(i+1, j, 0, stateFail)
+			visited[i+1][j] = false
+		}
+		if j+1 < rowL {
+			visited[i][j+1] = true
+			search(i, j+1, 0, stateFail)
+			visited[i][j+1] = false
+		}
 	}
+	search(0, 0, 0, stateFail)
 	return found
 }

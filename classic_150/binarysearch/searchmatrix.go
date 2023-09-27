5642184c0751f5729cf24aab7bb6848ff6ae85e8
AeaZer <3474974468@qq.com>
2023-09-27 18:49:32 +0800

    注释

--- classic_150/binarysearch/searchmatrix.go
+++ classic_150/binarysearch/searchmatrix.go
@@ -1,5 +1,13 @@
 package binarysearch
 
+// 74. 搜索二维矩阵
+// https://leetcode.cn/problems/search-a-2d-matrix/description/?envType=study-plan-v2&envId=top-interview-150
+
+/*给你一个满足下述两条属性的 m x n 整数矩阵：
+每行中的整数从左到右按非递减顺序排列。
+每行的第一个整数大于前一行的最后一个整数。
+给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。*/
+
 // [1,3,5,7],[10,11,16,20],[23,30,34,60]
 
 func searchMatrix(matrix [][]int, target int) bool {

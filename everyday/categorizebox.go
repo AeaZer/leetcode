b6e261f3a143e1d83d1d1f601c1bec1ef12e8bc6
AeaZer <3474974468@qq.com>
2023-10-20 10:54:20 +0800

    每日一题：2525. 根据规则将箱子分类

---
+++ everyday/categorizebox.go
@@ -0,0 +1,31 @@
+package everyday
+
+// 2525. 根据规则将箱子分类
+// https://leetcode.cn/problems/categorize-box-according-to-criteria/description/?envType=daily-question&envId=2023-10-20
+
+/*给你四个整数 length ，width ，height 和 mass ，分别表示一个箱子的三个维度和质量，请你返回一个表示箱子 类别 的字符串。
+
+如果满足以下条件，那么箱子是 "Bulky" 的：
+箱子 至少有一个 维度大于等于 104 。
+或者箱子的 体积 大于等于 109 。
+如果箱子的质量大于等于 100 ，那么箱子是 "Heavy" 的。
+如果箱子同时是 "Bulky" 和 "Heavy" ，那么返回类别为 "Both" 。
+如果箱子既不是 "Bulky" ，也不是 "Heavy" ，那么返回类别为 "Neither" 。
+如果箱子是 "Bulky" 但不是 "Heavy" ，那么返回类别为 "Bulky" 。
+如果箱子是 "Heavy" 但不是 "Bulky" ，那么返回类别为 "Heavy" 。
+注意，箱子的体积等于箱子的长度、宽度和高度的乘积。*/
+
+func categorizeBox(length int, width int, height int, mass int) string {
+	bulky := length >= 1e4 || width >= 1e4 || height >= 1e4 || length*width*height >= 1e9
+	heavy := mass >= 100
+	if bulky && heavy {
+		return "Both"
+	}
+	if bulky {
+		return "Bulky"
+	}
+	if heavy {
+		return "heavy"
+	}
+	return "Neither"
+}
